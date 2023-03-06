7a83e589bb6dcf792f4b42cd08114dc17c137f05
srijan0412 <95132945+srijan0412@users.noreply.github.com>
2023-03-06 20:15:02 +0530

    stack-basic

---
+++ Stack/basic_stack.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#define n 100
+using namespace std;
+
+class Stack {
+    int *arr;
+    int top;
+    public:
+
+    Stack(){
+        arr = new int[n];
+        top = -1;
+    }
+
+    void push(int x){
+        if (top==n-1){
+            cout << "Stack overflow" << endl;
+            return;
+        }
+
+        top++;
+        arr[top] = x;
+    }
+
+    void pop(){
+        if (top==-1){
+            cout << "No element to pop" << endl;
+            return;
+        }
+        top--;
+    }
+
+    int Top(){
+        if (top == -1){
+            cout << "No element in stack" << endl; 
+            return -1;
+        }
+        return arr[top];
+    }
+
+    bool empty(){
+        if (top == -1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+
+int main(){
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    // cout << st.Top() << endl;
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    cout << st.empty() << endl;
+
+    return 0;
+}
