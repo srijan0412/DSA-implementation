617b4b3a0b6bc9ab2af1e5bf39056e7f14e18858
srijan0412 <95132945+srijan0412@users.noreply.github.com>
2023-03-12 17:01:08 +0530

    binary_tree basics

--- Binary-Tree/binary_tree_basic2.cpp
+++ Binary-Tree/binary_tree_basic2.cpp
@@ -1,9 +1,10 @@
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
-// Input : 1 2 4 -1 -1 5 7 -1 -1 3 -1 6 -1 -1
+// Input : 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
 
+// Note class for the binary tree 
 class Node {
     public:
     int data;
@@ -18,21 +19,35 @@ class Node {
 
 // Building Tree through preorder traversal ( Node -> left -> right )
 // this is a recursive function :
-node* buildtree(){
-    int d;
-    cin >> d;
+Node* buildTree(){
+    int d; cin >> d;
 
     if (d == -1){
         return NULL;
     }
     
     Node* n = new Node(d);
-    n->left = buildtree();
-    n->right = buildtree();
+    n->left = buildTree();
+    n->right = buildTree();
+    cout << "running_buildtree" << endl;
     return n;
 }
 
+// prints all the nodes in Preorder traversal form 
+void printPreorder(Node* root){
+    if (root = NULL){
+        return;
+    }
+    // cout << "running_printpreorder" << endl;
+
+    cout << root->data << " ";
+    printPreorder(root->left);
+    printPreorder(root->right);
+}
+// Notrunning  
+
 int main(){
-    Node* root = buildtree();
+    Node* root = buildTree();
+    printPreorder(root);
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printBinary(int a){
-    for (int i=10; i>0; i--){
-        cout << (1<<i)&a;
-    }
-    cout << endl;
-}
-
 int main(){
-    int a = 5;
-    printBinary(a);
+    int a = NULL;
+    cout << a << endl;
     return 0;
 }
